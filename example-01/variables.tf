8540d4da11a9b30028764c5c0672663a3997d15e
Maxim Burunov <maxbur89@gmail.com>
2021-11-27 21:01:25 +0700

    add vars

--- example-01/variables.tf
+++ example-01/variables.tf
@@ -7,17 +7,17 @@ variable "token" {
 variable "cloud_id" {
   description = "cloud_id for access Yandex Cloud"
   type        = string
-  default     = "b1g3jddf4nv5e9okle7p"
+  default     = ""
 }
 
 variable "folder_id" {
   description = "folder_id for access Yandex Cloud"
   type        = string
-  default     = "b1gk960dgke8q6mb3j64"
+  default     = ""
 }
 
 variable "zone" {
   description = "zone for access Yandex Cloud"
   type        = string
-  default     = "ru-central1-b"
+  default     = ""
 }
