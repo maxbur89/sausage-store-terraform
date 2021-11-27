b60e30f3c1be415372d61383d615490878957bb5
Maxim Burunov <maxbur89@gmail.com>
2021-11-27 16:13:31 +0700

    commit

--- example-01/main.tf
+++ example-01/main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.67.0"
-    }
-  }
-}
-
-provider "yandex" {
-  token     = var.token
-  cloud_id  = var.cloud_id
-  folder_id = var.folder_id
-  zone      = var.zone
-}
-
 resource "yandex_compute_instance" "vm-burunov-m" {
   name                      = "terraform-burunov-m"
   allow_stopping_for_update = true
