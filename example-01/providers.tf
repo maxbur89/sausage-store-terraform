4709b181d0d34ee3089ec8c7d93219e4c8f9c951
Maxim Burunov <maxbur89@gmail.com>
2021-11-28 10:22:02 +0700

    Add providers.tf

--- example-01/providers.tf
+++ example-01/providers.tf
@@ -5,6 +5,16 @@ terraform {
       version = "0.67.0"
     }
   }
+
+  backend "s3" {
+    endpoint                    = "storage.yandexcloud.net"
+    bucket                      = "terraformstate-burunov"
+    region                      = "ru-central1-b"
+    key                         = "terraform.tfstate"
+    skip_region_validation      = true
+    skip_credentials_validation = true
+  }
+
 }
 
 provider "yandex" {
