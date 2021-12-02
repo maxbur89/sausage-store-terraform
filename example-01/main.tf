f1c8cbaf96df2ce01c76571895f66390166169cb
Maxim Burunov <maxbur89@gmail.com>
2021-12-02 18:08:26 +0700

    db

--- example-01/main.tf
+++ example-01/main.tf
@@ -34,3 +34,64 @@ resource "yandex_vpc_subnet" "subnet-terraform-burunov-m-1" {
   network_id     = yandex_vpc_network.network-terraform-burunov-m-1.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
+
+resource "yandex_mdb_postgresql_cluster" "burunov-pg" {
+  name        = "burunov-pg"
+  environment = "PRESTABLE"
+  network_id  = yandex_vpc_network.burunov-subnet-pg.id
+
+  config {
+    version = 13
+    resources {
+      resource_preset_id = "b2.nano"
+      disk_type_id       = "network-ssd"
+      disk_size          = 16
+    }
+    postgresql_config = {
+      max_connections                   = 50
+      enable_parallel_hash              = true
+      vacuum_cleanup_index_scale_factor = 0.2
+      autovacuum_vacuum_scale_factor    = 0.34
+      default_transaction_isolation     = "TRANSACTION_ISOLATION_READ_COMMITTED"
+      shared_preload_libraries          = "SHARED_PRELOAD_LIBRARIES_AUTO_EXPLAIN,SHARED_PRELOAD_LIBRARIES_PG_HINT_PLAN"
+    }
+  }
+
+  maintenance_window {
+    type = "WEEKLY"
+    day  = "SAT"
+    hour = 12
+  }
+
+  database {
+    name  = "sausage"
+    owner = "sausage"
+  }
+
+  user {
+    name       = "sausage"
+    password   = "your_password"
+    conn_limit = 50
+    permission {
+      database_name = "sausage"
+    }
+    settings = {
+      default_transaction_isolation = "read committed"
+      log_min_duration_statement    = 5000
+    }
+  }
+
+  host {
+    zone             = "ru-central1-a"
+    subnet_id        = yandex_vpc_subnet.burunov-subnet-pg.id
+    assign_public_ip = true
+  }
+}
+
+resource "yandex_vpc_network" "burunov-subnet-pg" {}
+
+resource "yandex_vpc_subnet" "burunov-subnet-pg" {
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.burunov-subnet-pg.id
+  v4_cidr_blocks = ["10.5.0.0/24"]
+}
