64c7de23e27a53dbd1fcfc8c351157d9db78ad46
Maxim Burunov <maxbur89@gmail.com>
2022-01-28 01:21:23 +0700

    fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,12 +8,17 @@ terraform {
 
   backend "s3" {
     endpoint = "storage.yandexcloud.net"
-    bucket   = "terraformstate1"
+    bucket   = "burunov-bucket-private"
     region   = "ru-central1-b"
     key      = "terraform.tfstate"
 
-    #  #   access_key = var.access_key
-    #  #   secret_key = var.secret_key
+    #access_key = var.access_key
+    #secret_key = var.secret_key
+    #or
+    #export AWS_ACCESS_KEY_ID=""
+    #export AWS_SECRET_ACCESS_KEY=""
+    #export TF_VAR_token=
+
 
     skip_region_validation      = true
     skip_credentials_validation = true
