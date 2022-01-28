e71190f72c3a2ea95a112eb4dcfee76e4f84a7bd
Maxim Burunov <maxbur89@gmail.com>
2022-01-28 19:37:46 +0700

    fmt

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -12,12 +12,14 @@ terraform {
     region   = "ru-central1-b"
     key      = "terraform.tfstate"
 
-    #access_key = var.access_key
-    #secret_key = var.secret_key
-    #or
-    #export AWS_ACCESS_KEY_ID=""
-    #export AWS_SECRET_ACCESS_KEY=""
-    #export TF_VAR_token=
+    # access_key = var.access_key
+    # secret_key = var.secret_key
+    # or
+    # логин и пароль сервисного аккаунта yandex cloud
+    # export AWS_ACCESS_KEY_ID=""
+    # export AWS_SECRET_ACCESS_KEY=""
+    # https://oauth.yandex.ru/
+    # export TF_VAR_token=
 
 
     skip_region_validation      = true
